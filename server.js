1c301d07e496f0a49b68c78250b050d2bc7dbbdc
losaugrumov1-netizen <losaugrumov1@gmail.com>
2026-08-20 21:56:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const PORT = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
+console.log("ROOT:", __dirname);
+console.log("INDEX:", path.join(__dirname, "index.html"));
+
 // Отдаём весь сайт из корня проекта
 app.use(express.static(__dirname));
 
